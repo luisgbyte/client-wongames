246344c6a552227b8ff67c094c4bbe1f4c748b4e
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-01 11:32:48 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+
+module.exports = {
+  reactStrictMode: true
+}
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,12 @@
+import Main from '.'
+
+import { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'Main',
+  component: Main
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+
+export const Default: Story = (args) => <Main {...args} />
