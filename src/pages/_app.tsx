67dd138af3989d0fa5431c8a49a2975156de901f
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-09-02 17:29:03 -0300

    add ThemeProvider and styled-components.d.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
@@ -19,7 +21,7 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
