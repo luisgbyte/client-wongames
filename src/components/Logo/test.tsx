e500869fd9c992f7e2d31c88b93608019749a691
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-10-18 16:25:33 -0300

    add option to size on Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -5,10 +5,6 @@ import Logo from '.'
 
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
-    // renderizar o componente `render`
-    // selecionar o elemento a ser testado `screen` (queries) = getByLabel...
-    // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
-
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
@@ -19,6 +15,20 @@ describe('<Logo />', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
