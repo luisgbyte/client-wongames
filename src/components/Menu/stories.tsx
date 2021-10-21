4c6804927a3c4147db90e22036ad61c168ae13e5
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-10-21 12:43:36 -0300

    add Menu mobile

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,10 @@
+import Menu from '.'
+
+import { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+export const Basic: Story = () => <Menu />
