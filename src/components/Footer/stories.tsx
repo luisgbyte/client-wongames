d34c53362f62885d48af3fe17a7f898850795d19
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-10-26 17:34:24 -0300

    add footer

---
+++ src/components/Footer/stories.tsx
@@ -0,0 +1,14 @@
+import Footer from '.'
+
+import { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'Footer',
+  component: Footer
+} as Meta
+
+export const Basic: Story = () => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <Footer />
+  </div>
+)
