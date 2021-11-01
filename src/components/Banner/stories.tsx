14b04c10bdb69f76143c26a4f9055a08d32660ed
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-01 09:10:15 -0300

    add Banner structure

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,17 @@
+import Banner, { BannerProps } from '.'
+
+import { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+} as Meta
+
+export const Basic: Story<BannerProps> = (args) => <Banner {...args} />
