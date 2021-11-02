13fe89f924f850d12b9a521cad4aca558dc0df27
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-02 20:26:47 -0300

    add Ribbon to Banner implementation

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -17,4 +17,20 @@ export default {
   }
 } as Meta
 
-export const Basic: Story<BannerProps> = (args) => <Banner {...args} />
+export const Basic: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+export const WithRibbon: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary'
+}
