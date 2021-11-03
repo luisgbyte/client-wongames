68f6cfa49097d1aca5f1d18e4d5be69bb8f67e81
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-03 15:22:03 -0300

    add GameCard structure

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,20 @@
+import GameCard, { GameCardProps } from '.'
+
+import { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstart Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00'
+  }
+} as Meta
+
+export const Basic: Story<GameCardProps> = (args) => (
+  <div style={{ maxWidth: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import GameCard from '.'
+
+describe('<GameCard />', () => {
+  it('should render correctly', () => {
+    //renderizar o GameCard
+    //verifique se o title foi renderizado
+    //verifique se a developer foi renderizada
+    //verifique se a img foi renderizada
+    //verifique se a price foi renderizada
+  })
+})
