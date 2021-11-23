21e5d097b655ace6dc7fa9dcfe44ae43538b177a
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-23 20:13:32 -0300

    add Button minimal

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -53,6 +53,17 @@ describe('<Button />', () => {
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 
+  it('should render a minimal version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />} minimal>
+        Buy now
+      </Button>
+    )
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      background: 'none'
+    })
+  })
+
   it('should render Button as a link', () => {
     renderWithTheme(
       <Button as="a" href="/link">
