9088acc58e9c8c1f44022ee7bbe8c712891fd445
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-25 15:30:48 -0300

    add styles to input on Checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -8,7 +8,7 @@ export type CheckboxProps = {
 
 const Checkbox = ({ label, labelFor = 'white', labelColor }: CheckboxProps) => (
   <S.Wrapper>
-    <input id={labelFor} type="checkbox" />
+    <S.Input id={labelFor} type="checkbox" />
     {!!label && (
       <S.Label htmlFor={labelFor} labelColor={labelColor}>
         {label}
