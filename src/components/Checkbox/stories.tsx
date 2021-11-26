f29e25440c4728cec1aa91e7acbc7201761bd98f
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-26 14:58:10 -0300

    refine stories, add acessibility test and snapshot to Checkbox

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -5,11 +5,45 @@ import { Story, Meta } from '@storybook/react'
 export default {
   title: 'Checkbox',
   component: Checkbox,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
   argTypes: {
     onCheck: { action: 'checked' }
   }
 } as Meta
 
 export const Basic: Story<CheckboxProps> = (args) => (
-  <Checkbox isChecked {...args} />
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Action"
+        labelFor="action"
+        isChecked
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Adventure"
+        labelFor="adventure"
+        isChecked
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Strategy"
+        labelFor="strategy"
+        isChecked
+        {...args}
+      />
+    </div>
+  </>
 )
