cbaa22ac5bd5b742df1cb623cc57feeefa2abf0a
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-12-09 20:06:54 -0300

    organize storybook Form items

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -3,7 +3,7 @@ import Checkbox, { CheckboxProps } from '.'
 import { Story, Meta } from '@storybook/react'
 
 export default {
-  title: 'Checkbox',
+  title: 'Form/Checkbox',
   component: Checkbox,
   parameters: {
     layout: 'fullscreen',
