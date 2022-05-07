9850ff91364281e813f1b14d349dc395e05d15a9
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-05-07 14:34:53 -0300

    add FormSignIn tests

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,20 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    // verificar email
-    // verificar password
-    // verificar button
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
   })
 
   it('should render the forgot password link', () => {
-    // teste
+    renderWithTheme(<FormSignIn />)
   })
 
   it('should render the text and link to sign up', () => {
-    // verificar o texto
-    // verificar o link
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+    expect(screen.getByText(/don`t have an account?\?/i)).toBeInTheDocument()
   })
 })
