3625529ef243f4cb1905f418f44f2475d2d20685
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-05-12 20:59:05 -0300

    add html and tests to GameInfo

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,10 +1,19 @@
-import GameInfo from '.'
+import GameInfo, { GameInfoProps } from '.'
 
 import { Story, Meta } from '@storybook/react'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo
+  component: GameInfo,
+  args: {
+    title: 'Borderlands',
+    description: 'Game Description',
+    price: '210,00'
+  }
 } as Meta
 
-export const Basic: Story = () => <GameInfo />
+export const Basic: Story<GameInfoProps> = (args) => (
+  <div>
+    <GameInfo {...args} />
+  </div>
+)
--- src/components/GameInfo/styles.ts
+++ src/components/GameInfo/styles.ts
@@ -1,3 +1,5 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+export const Description = styled.p``
+export const ButtonWrapper = styled.div``
