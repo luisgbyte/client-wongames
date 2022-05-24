4c3710169e2c4a3623517ce6120a96e2a48bfee2
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-05-24 18:32:43 -0300

    add initial Gallery structure

---
+++ src/components/Gallery/stories.tsx
@@ -0,0 +1,23 @@
+import Gallery, { GalleryProps } from '.'
+import { Story, Meta } from '@storybook/react'
+import items from './mock'
+
+export default {
+  title: 'Gallery',
+  component: Gallery,
+  args: {
+    items
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Basic: Story<GalleryProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <Gallery {...args} />
+  </div>
+)
