d65ef9026c1a05c70535446ff611aa59919a35f2
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-05-26 15:53:00 -0300

    add GameInfo to Game page

--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -1,6 +1,16 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
+import { Container } from 'components/Container'
+
+export const Main = styled.main`
+  margin-top: 20rem;
+
+  ${media.greaterThan('medium')`
+    margin-top: 58rem;
+  `}
+`
+
 type CoverProps = {
   src: string
 }
@@ -13,12 +23,24 @@ export const Cover = styled.div<CoverProps>`
     left: 0;
     height: 39.5rem;
     background-image: url(${src});
-    background-size: cover center;
+    background-size: cover;
+    background-position: top center;
     opacity: 0.4;
-
     ${media.greaterThan('medium')`
       height: 70rem;
       clip-path: polygon(0 0, 100% 0, 100% 100%, 0 85%);
     `}
   `}
 `
+
+const Section = styled(Container).attrs({ as: 'section' })`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.xlarge};
+
+    ${media.greaterThan('medium')`
+      margin-bottom: calc(${theme.spacings.xlarge}*2);
+    `}
+  `}
+`
+
+export const SectionGameInfo = styled(Section)``
