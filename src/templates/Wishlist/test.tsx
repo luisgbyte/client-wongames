f9b31f3e89375d826076da4b7c02266204ff1397
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-05-27 19:42:42 -0300

    add games to Wishlist Page

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -8,6 +8,7 @@ import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 const props = {
+  games: gamesMock,
   recommendedHighlight: highlightMock,
   recommendedGames: gamesMock
 }
@@ -27,6 +28,7 @@ describe('<Wishlist />', () => {
       screen.getByRole('heading', { name: /Wishlist/i })
     ).toBeInTheDocument()
 
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(6)
     expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
   })
 })
