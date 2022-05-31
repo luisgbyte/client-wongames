fabf4fe58de5895775479d0ee4ab7be3ed5ac4e2
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-05-30 21:23:22 -0300

    add PaymentOptions styles

--- src/components/PaymentOptions/stories.tsx
+++ src/components/PaymentOptions/stories.tsx
@@ -9,9 +9,24 @@ export default {
   component: PaymentOptions,
   args: {
     cards: cardsMock
+  },
+  argTypes: {
+    cards: {
+      type: ''
+    },
+    handlePayment: {
+      action: 'clicked'
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
   }
 } as Meta
 
 export const Basic: Story<PaymentOptionsProps> = (args) => (
-  <PaymentOptions {...args} />
+  <div style={{ padding: 16, maxWidth: 400 }}>
+    <PaymentOptions {...args} />
+  </div>
 )
