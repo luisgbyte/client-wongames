89f3f2183bbec8a7805b2fd7f2b34299242e2fb3
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-06-10 21:39:33 -0300

    add FormProfile structure

---
+++ src/components/FormProfile/stories.tsx
@@ -0,0 +1,10 @@
+import FormProfile from '.'
+
+import { Story, Meta } from '@storybook/react'
+
+export default {
+  title: 'FormProfile',
+  component: FormProfile
+} as Meta
+
+export const Basic: Story = () => <FormProfile />
