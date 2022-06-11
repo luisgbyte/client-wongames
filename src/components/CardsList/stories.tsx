f7e8ecfbabc32d0f5f33e66948d8e5451049f301
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-06-11 15:26:50 -0300

    add Profile Template and /profile/me page

--- src/components/CardsList/stories.tsx
+++ src/components/CardsList/stories.tsx
@@ -1,7 +1,7 @@
-import { Story, Meta } from '@storybook/react'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import CardsList, { CardsListProps } from '.'
 
-import cardsMock from 'components/PaymentOptions'
+import cardsMock from 'components/PaymentOptions/mock'
 
 export default {
   title: 'Profile/CardsList',
@@ -11,7 +11,7 @@ export default {
   }
 } as Meta
 
-export const Basic: Story<CardsListProps> = (args) => (
+export const Default: Story<CardsListProps> = (args) => (
   <div style={{ maxWidth: 850, margin: 'auto' }}>
     <CardsList {...args} />
   </div>
---
+++ src/pages/profile/orders.tsx
@@ -0,0 +1,3 @@
+export default function Orders() {
+  return <h3>Orders</h3>
+}
