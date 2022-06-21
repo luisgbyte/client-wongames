5e788006ae801b83925ad0b29fff70d08e2eaf2e
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-06-21 16:00:17 -0300

    adjust Menu to have correct items and dropdown

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -4,14 +4,19 @@ import { Story, Meta } from '@storybook/react'
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
 export const Basic: Story<MenuProps> = (args) => <Menu {...args} />
 
-Basic.parameters = {
-  layout: 'fullscreen',
-  backgrounds: {
-    default: 'won-dark'
-  }
+export const Logged: Story<MenuProps> = (args) => <Menu {...args} />
+
+Logged.args = {
+  username: 'John Dow'
 }
