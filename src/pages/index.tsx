f7cf01dc976ecdac7ad113a07c5674d85f4699b5
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2022-06-23 16:34:27 -0300

    add ApolloProvider and first useQuery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { gql, useQuery } from '@apollo/client'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import hightligthMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const { data, loading, error } = useQuery(gql`
+    query getGame {
+      games {
+        name
+      }
+    }
+  `)
+
+  if (loading) return <p>Loading...</p>
+
+  if (error) return <p>{error}</p>
+
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
+
   return <Home {...props} />
 }
 
